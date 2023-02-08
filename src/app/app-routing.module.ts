b3412742fe6a30a9b2ea6b52c93a996437ec9496
jadroweb <jadroweb@github.com>
2023-02-08 15:57:09 +0100

    Add lazy routing

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,20 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { MainPageComponent } from './components/main-page/main-page.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {
+    path: '',
+    component: MainPageComponent,
+    pathMatch: 'full'
+  },
+  {
+    path: 'product/:productId',
+    loadChildren: () =>
+      import('./components/product/product.module').then(module => module.ProductModule)
+  },
+  { path: '**', redirectTo: '' }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
