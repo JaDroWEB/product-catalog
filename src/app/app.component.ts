b5a4e39517b5a197ee73349765e8a80745af2a1a
jadroweb <jadroweb@github.com>
2023-02-08 16:00:18 +0100

    Set app module and component

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -5,6 +5,4 @@ import { Component } from '@angular/core';
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss']
 })
-export class AppComponent {
-  title = 'product-catalog';
-}
+export class AppComponent {}
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,32 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-
 import { AppRoutingModule } from './app-routing.module';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { HttpClientModule } from '@angular/common/http';
+
+// Components
 import { AppComponent } from './app.component';
+import { MenuItemsComponent } from './components/main-page/menu-items/menu-items.component';
+import { MainPageComponent } from './components/main-page/main-page.component';
+import { AkitaNgDevtools } from '@datorama/akita-ngdevtools';
+import { environment } from 'src/environments/environment';
+import { SharedModule } from './shared.module';
+import { ProductItemsComponent } from './components/main-page/product-items/product-items.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    MenuItemsComponent,
+    MainPageComponent,
+    ProductItemsComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    BrowserAnimationsModule,
+    HttpClientModule,
+    environment.production ? [] : AkitaNgDevtools.forRoot(),
+    SharedModule
   ],
   providers: [],
   bootstrap: [AppComponent]
