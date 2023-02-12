14edd8c6757b87c42579716ada8b7601f711c9f1
jadroweb <jadroweb@github.com>
2023-02-12 20:53:45 +0100

    Use async pipe instead of subscribe in code

--- src/app/components/product/product.component.ts
+++ src/app/components/product/product.component.ts
@@ -3,28 +3,21 @@ import { Location } from '@angular/common';
 import { MatDialog } from '@angular/material/dialog';
 import { ProductQuery } from 'src/app/services/product/product.query';
 import { EditProductComponent } from './edit-product/edit-product.component';
-import { Product } from 'src/app/services/product/product';
-import { UntilDestroy, untilDestroyed } from '@ngneat/until-destroy';
+import { shareReplay, tap } from 'rxjs';
 
-@UntilDestroy()
 @Component({
   selector: 'app-product',
   templateUrl: './product.component.html',
   styleUrls: ['./product.component.scss']
 })
 export class ProductComponent {
-  product!: Product | undefined;
+  product = this.productQuery.selectActive().pipe(shareReplay());
 
   constructor(
     private productQuery: ProductQuery,
     private dialog: MatDialog,
     private location: Location
-  ) {
-    this.productQuery
-      .selectActive()
-      .pipe(untilDestroyed(this))
-      .subscribe(prod => this.product = prod)
-  }
+  ) {}
 
   edit() {
     this.dialog.open(EditProductComponent, {
